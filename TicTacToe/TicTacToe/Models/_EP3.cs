3fdbbaf568f069c4170629d6a10689d772f960b4
Noman Sanjari <nomansanjari2001@gmail.com>
2022-02-10 16:45:30 -0700

    Endpoint 3 done

---
+++ TicTacToe/TicTacToe/Models/_EP3.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe.Models
+{
+    public class _EP3
+    {
+        public int GameID { get; set; }
+        public string PlayerA{ get; set; }
+        public string PlayerB { get; set; }
+        public int Moves { get; set; }
+    }
+}
